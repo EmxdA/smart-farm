2c72eec70d029b8564e097af454a35080f2111ef
Emad <emad.321a@gmail.com>
2022-01-20 14:58:00 +1100

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import { Component } from "react/cjs/react.production.min";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+  useMap,
+  LayersControl,
+  LayerGroup,
+} from "react-leaflet";
+import { Map, popup } from "leaflet";
+import React from "react";
 
-import "./App.css"
+import "./App.css";
+import { click } from "@testing-library/user-event/dist/click";
+// import { DisplayPosition } from "./map";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       data: [],
+      forest1: [],
+      forest2: [],
       isLoaded: false,
       dataState: "",
+      center: [51.505, -0.091],
+      zoom: 5,
+      marker: null,
     };
   }
 
@@ -27,8 +45,26 @@ class App extends Component {
   }
 
   render() {
-    var { isLoaded, data, dataState } = this.state;
+    var { isLoaded, data, dataState, marker, zoom, center, forest1, forest2 } =
+      this.state;
+    // var map = this.map;
     var test = data.slice(0, 30);
+    var farm1 = data.slice(0, 10);
+    var farm2 = data.slice(11, 20);
+    var farm3 = data.slice(21, 30);
+
+    function LocationMarker() {
+      const map = useMapEvents({
+        click() {
+          map.locate();
+        },
+        locationfound(e) {
+          map.flyTo(center, zoom);
+        },
+      });
+
+      return null;
+    }
 
     if (!isLoaded) {
       return <div>Loading.....</div>;
@@ -40,47 +76,136 @@ class App extends Component {
               type="text"
               placeholder="search..."
               onChange={(event) => {
-                this.setState({ dataState: event.target.value});
+                this.setState({ dataState: event.target.value });
               }}
             />
+
+            <br></br>
+            <button
+              id="reset-btn"
+              onClick={() => {
+                this.setState({
+                  center: [50, -1],
+                  zoom: 5,
+                });
+                console.log(center, zoom);
+              }}
+            >
+              Reset Marker
+            </button>
+
             <div id="view">
-              {test.filter((test) => {
+              {test
+                .filter((test) => {
                   if (dataState === "") {
                     return test;
-                  } else if (test.location.street.name.toLowerCase().includes(dataState.toLowerCase())) {
+                  } else if (
+                    test.location.street.name
+                      .toLowerCase()
+                      .includes(dataState.toLowerCase())
+                  ) {
                     return dataState;
                   } else {
                     return null;
                   }
                 })
                 .map((test) => (
-                  <p display="none" key={test.id}>
+                  <p display="none" key={test.id} id="side-info">
                     ID: {test.id} <br></br>
                     Street Name: {test.location.street.name}
                     <br></br>
-                    <button>View</button>
+                    <button
+                      onClick={() => {
+                        this.setState({
+                          center: [
+                            Number(test.location.latitude),
+                            Number(test.location.longitude),
+                          ],
+                          zoom: 18,
+                        });
+                        console.log(center, zoom);
+                      }}
+                    >
+                      Set Marker
+                    </button>
                   </p>
                 ))}
+            </div>
           </div>
-          </div>
-          <MapContainer id="map" center={[51.505, -0.09]} zoom={5}>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {test.map((test) => (
-              <Marker
-                key={test.id}
-                position={[test.location.latitude, test.location.longitude]}
-              >
-                {" "}
-                <Popup>
-                  <p>ID: {test.id}</p>
-                  <p>Longitude: {test.location.longitude}</p>
-                  <p>Latitude: {test.location.latitude} </p>
-                </Popup>
-              </Marker>
-            ))}
+          <MapContainer id="map" center={center} zoom={zoom}>
+            <button id="trigger">
+              Travel To Set Marker!
+              <LocationMarker />
+            </button>
+            <LayersControl position="topright">
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+
+              <LayersControl.Overlay name="Farm 1">
+                <LayerGroup>
+                  {farm1.map((test) => (
+                    <Marker
+                      key={test.id}
+                      position={[
+                        test.location.latitude,
+                        test.location.longitude,
+                      ]}
+                    >
+                      {" "}
+                      <Popup>
+                        <p>ID: {test.id}</p>
+                        <p>Longitude: {test.location.longitude}</p>
+                        <p>Latitude: {test.location.latitude} </p>
+                      </Popup>
+                    </Marker>
+                  ))}
+                </LayerGroup>
+              </LayersControl.Overlay>
+
+              <LayersControl.Overlay name="Farm 2">
+                <LayerGroup>
+                  {farm2.map((test) => (
+                    <Marker
+                      key={test.id}
+                      position={[
+                        test.location.latitude,
+                        test.location.longitude,
+                      ]}
+                    >
+                      {" "}
+                      <Popup>
+                        <p>ID: {test.id}</p>
+                        <p>Longitude: {test.location.longitude}</p>
+                        <p>Latitude: {test.location.latitude} </p>
+                      </Popup>
+                    </Marker>
+                  ))}
+                </LayerGroup>
+              </LayersControl.Overlay>
+
+              <LayersControl.Overlay name="Farm 3">
+                <LayerGroup>
+                  {farm3.map((test) => (
+                    <Marker
+                      key={test.id}
+                      position={[
+                        test.location.latitude,
+                        test.location.longitude,
+                      ]}
+                    >
+                      {" "}
+                      <Popup>
+                        <p>ID: {test.id}</p>
+                        <p>Longitude: {test.location.longitude}</p>
+                        <p>Latitude: {test.location.latitude} </p>
+                      </Popup>
+                    </Marker>
+                  ))}
+                </LayerGroup>
+              </LayersControl.Overlay>
+            </LayersControl>
           </MapContainer>
         </div>
       );
